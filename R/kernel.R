97d7a071b3feb71e6774231fc02c4ad37ecc059e
Jon Clayden <jon.clayden@gmail.com>
2012-04-25 12:58:54 +0100

    Various updates to kernel functions

--- R/kernel.R
+++ R/kernel.R
@@ -1,30 +1,66 @@
 shapeKernel <- function (width, dim = length(width), type = c("box","disc","diamond"), brush = TRUE, binary = TRUE, normalised = FALSE)
 {
     type <- match.arg(type)
+    bg <- ifelse(brush, NA, 0)
     
     if (dim > length(width))
         width <- rep(width, length.out=dim)
     
+    maxWidth <- ceiling(max(width))
+    scaleFactors <- max(width) / width
+    
     # This corresponds to the smallest isotropic kernel of odd width to fit the shape
-    size <- max(ifelse(width %% 2 == 1, width, width+1))
-    kernel <- array(ifelse(brush,NA,0), dim=rep(size,dim))
+    size <- ifelse(maxWidth %% 2 == 1, maxWidth, maxWidth+1)
+    kernel <- array(bg, dim=rep(size,dim))
     
     x <- 1:size - (size+1)/2
-    x <- lapply(max(width)/width, "*", x)
+    nearEdges <- lapply(scaleFactors, "*", x - 0.5*sign(x))
+    farEdges <- lapply(scaleFactors, "*", x + 0.5*sign(x))
+    
     normFun <- switch(type, box=function(a,b) pmax(abs(a),abs(b)),
                             disc=function(a,b) sqrt(a^2 + b^2),
                             diamond=function(a,b) (abs(a) + abs(b)))
-    norms <- Reduce(function(a,b) outer(a,b,FUN=normFun), x)
+    
+    minNorms <- Reduce(function(a,b) outer(a,b,FUN=normFun), nearEdges)
+    maxNorms <- Reduce(function(a,b) outer(a,b,FUN=normFun), farEdges)
+    
+    maxDistance <- max(width) / 2
+    indices <- minNorms < maxDistance
+    kernel[indices] <- pmin(1, (maxDistance - minNorms[indices]) / (maxNorms[indices] - minNorms[indices]))
     
     if (binary)
-        kernel <- ifelse(kernel==0, 0L, 1L)
+        kernel <- ifelse(kernel < 0.5, as.integer(bg), 1L)
     else if (normalised)
-        kernel <- kernel / sum(kernel)
+        kernel <- kernel / sum(kernel, na.rm=TRUE)
     
     return (kernel)
 }
 
-gaussianKernel <- function (sigma, dim = length(sigma), size = NULL)
+gaussianKernel <- function (sigma, dim = length(sigma), size = NULL, normalised = TRUE)
 {
+    if (dim > length(sigma))
+        sigma <- rep(sigma, length.out=dim)
     
+    if (is.null(size))
+        size <- ceiling(4 * max(sigma))
+    else if (length(size) > 1)
+        report(OL$Error, "The kernel size must be given as a single integer")
+    else
+        size <- ceiling(size)
+    
+    size <- ifelse(size %% 2 == 1, size, size+1)
+    
+    scaleFactors <- max(sigma) / sigma
+    x <- 1:size - (size+1)/2
+    centres <- lapply(scaleFactors, "*", x)
+    
+    normFun <- function(a,b) sqrt(a^2 + b^2)
+    norms <- Reduce(function(a,b) outer(a,b,FUN=normFun), centres)
+    
+    kernel <- array(dnorm(norms,sd=max(sigma)), dim=rep(size,dim))
+    
+    if (normalised)
+        kernel <- kernel / sum(kernel, na.rm=TRUE)
+    
+    return (kernel)
 }
