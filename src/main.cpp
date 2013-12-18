933f7e988a9381e2841b5ebb00e7b0fc7bf371cf
Jon Clayden <jon.clayden@gmail.com>
2013-12-18 15:41:19 +0000

    Exposing a function to R for sampling from a kernel

--- src/main.cpp
+++ src/main.cpp
@@ -19,25 +19,11 @@ Array * arrayFromData (SEXP data_)
     }
         
     Array *array = new Array(as<dbl_vector>(data), dim);
-    return (array);
+    return array;
 }
 
-RcppExport SEXP get_neighbourhood (SEXP data_, SEXP width_)
+Kernel * kernelFromElements (SEXP kernel_)
 {
-BEGIN_RCPP
-    Array *array = arrayFromData(data_);
-    
-    Neighbourhood neighbourhood = array->getNeighbourhood(as<int>(width_));
-    
-    return List::create(Named("width")=neighbourhood.width, Named("size")=neighbourhood.size, Named("locs")=neighbourhood.locs, Named("offsets")=neighbourhood.offsets);
-END_RCPP
-}
-
-RcppExport SEXP resample (SEXP data_, SEXP kernel_, SEXP samplingScheme_)
-{
-BEGIN_RCPP
-    Array *array = arrayFromData(data_);
-    
     List kernelElements(kernel_);
     string kernelName = as<string>(kernelElements["name"]);
     Kernel *kernel;
@@ -49,6 +35,43 @@ BEGIN_RCPP
     else if (kernelName.compare("mitchell-netravali") == 0)
         kernel = KernelGenerator::mitchellNetravali(as<double>(kernelElements["B"]), as<double>(kernelElements["C"]));
     
+    return kernel;
+}
+
+RcppExport SEXP get_neighbourhood (SEXP data_, SEXP width_)
+{
+BEGIN_RCPP
+    Array *array = arrayFromData(data_);
+    
+    Neighbourhood neighbourhood = array->getNeighbourhood(as<int>(width_));
+    
+    delete array;
+    
+    return List::create(Named("width")=neighbourhood.width, Named("size")=neighbourhood.size, Named("locs")=neighbourhood.locs, Named("offsets")=neighbourhood.offsets);
+END_RCPP
+}
+
+RcppExport SEXP sample_kernel (SEXP kernel_, SEXP values_)
+{
+BEGIN_RCPP
+    Kernel *kernel = kernelFromElements(kernel_);
+    NumericVector values(values_);
+    NumericVector result(values.length());
+    
+    for (int i=0; i<values.length(); i++)
+        result[i] = kernel->evaluate(values[i]);
+    
+    delete kernel;
+    
+    return result;
+END_RCPP
+}
+
+RcppExport SEXP resample (SEXP data_, SEXP kernel_, SEXP samplingScheme_)
+{
+BEGIN_RCPP
+    Array *array = arrayFromData(data_);
+    Kernel *kernel = kernelFromElements(kernel_);
     Resampler resampler(array, kernel);
     
     List samplingScheme(samplingScheme_);
