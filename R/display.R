4021715718a50829a0427820d1ad0c236cbcb6e1
Jon Clayden <jon.clayden@gmail.com>
2014-01-09 17:16:29 +0000

    Using rasterImage for speed

--- R/display.R
+++ R/display.R
@@ -3,21 +3,22 @@ display <- function (x, ...)
     UseMethod("display")
 }
 
-display.default <- function (x, transpose = TRUE, add = FALSE, ...)
+display.default <- function (x, transpose = TRUE, useRaster = TRUE, add = FALSE, ...)
 {
     x <- as.matrix(x)
     if (transpose)
         x <- t(x)
     
     col <- grey(0:255/255)
+    dpi <- 72
     
     if (add)
-        image(x[1:nrow(x),ncol(x):1], col=col, add=TRUE, ...)
+        image(x[1:nrow(x),ncol(x):1], col=col, useRaster=useRaster, add=TRUE, ...)
     else
     {
-        dev.new(width=nrow(x)/72, height=ncol(x)/72, dpi=72)
+        dev.new(width=nrow(x)/dpi, height=ncol(x)/dpi, dpi=dpi)
         oldPars <- par(mai=c(0,0,0,0), bg="grey70")
-        image(x[1:nrow(x),ncol(x):1], col=col, asp=ncol(x)/nrow(x), add=FALSE, ...)
+        image(x[1:nrow(x),ncol(x):1], col=col, asp=ncol(x)/nrow(x), useRaster=useRaster, add=FALSE, ...)
         par(oldPars)
     }
     
