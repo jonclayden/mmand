1d77824cb46f26d4463d96fdb0b9a585a5ec98f4
Jon Clayden <jon.clayden@gmail.com>
2014-04-10 17:00:48 +0100

    DPI checking improvements

--- R/display.R
+++ R/display.R
@@ -1,4 +1,23 @@
-.Workspace <- new.env()
+.Cache <- new.env()
+
+.checkDpi <- function ()
+{
+    if (!exists("dpiDevice",.Cache) || !identical(.Cache$dpiDevice,getOption("device")))
+    {
+        .Cache$dpiDevice <- getOption("device")
+        deviceFunction <- try(match.fun(.Cache$dpiDevice), silent=TRUE)
+        if ((class(deviceFunction) == "function") && ("dpi" %in% names(formals(deviceFunction))))
+            .Cache$dpi <- structure(c(72,72), explicit=TRUE)
+        else
+        {
+            dev.new(width=1, height=1)
+            .Cache$dpi <- structure(dev.size(units="px"), explicit=FALSE)
+            dev.off()
+        }
+    }
+    
+    return (.Cache$dpi)
+}
 
 display <- function (x, ...)
 {
@@ -11,20 +30,18 @@ display.default <- function (x, transpose = TRUE, useRaster = TRUE, add = FALSE,
     if (transpose)
         x <- t(x)
     
+    dpi <- .checkDpi()
     col <- grey(0:255/255)
-    
-    if (!exists("dpi",.Workspace) || is.null(.Workspace$dpi))
-    {
-        dev.new(width=1, height=1)
-        .Workspace$dpi <- dev.size(units="px")
-        dev.off()
-    }
     
     if (add)
         image(x[1:nrow(x),ncol(x):1], col=col, useRaster=useRaster, add=TRUE, ...)
     else
     {
-        dev.new(width=nrow(x)/.Workspace$dpi[1], height=ncol(x)/.Workspace$dpi[2])
+        if (attr(dpi,"explicit") && dpi[1] == dpi[2])
+            dev.new(width=nrow(x)/dpi[1], height=ncol(x)/dpi[2], dpi=dpi[1])
+        else
+            dev.new(width=nrow(x)/dpi[1], height=ncol(x)/dpi[2])
+        
         oldPars <- par(mai=c(0,0,0,0), bg="grey70")
         image(x[1:nrow(x),ncol(x):1], col=col, asp=ncol(x)/nrow(x), useRaster=useRaster, add=FALSE, ...)
         par(oldPars)
