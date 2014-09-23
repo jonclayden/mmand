58cb4bf30c3f11502cbca491adb1bbc2822a492a
Jon Clayden <code@clayden.org>
2014-09-23 14:42:45 +0100

    Replacing test image with a less traditional, but also less sexist, alternative

--- tests/testthat/test-10-morph.R
+++ tests/testthat/test-10-morph.R
@@ -1,5 +1,5 @@
 library(png)
-lena <- readPNG(system.file("images", "lena-small.png", package="mmand"))
+fan <- readPNG(system.file("images", "fan-small.png", package="mmand"))
 
 context("Mathematical morphology and filtering")
 
@@ -28,10 +28,10 @@ test_that("greyscale mathematical morphology operations work", {
     expect_that(dilate(data,kernel), equals(c(1,1,1.5,1,1,1,1.2,1.5,1.3,1,1)))
     
     kernel <- shapeKernel(c(3,3), type="diamond")
-    expect_that(erode(lena,kernel), equals_reference("lena_eroded.rds"))
-    expect_that(dilate(lena,kernel), equals_reference("lena_dilated.rds"))
-    expect_that(closing(lena,kernel), equals_reference("lena_opened.rds"))
-    expect_that(opening(lena,kernel), equals_reference("lena_closed.rds"))
+    expect_that(erode(fan,kernel), equals_reference("fan_eroded.rds"))
+    expect_that(dilate(fan,kernel), equals_reference("fan_dilated.rds"))
+    expect_that(closing(fan,kernel), equals_reference("fan_opened.rds"))
+    expect_that(opening(fan,kernel), equals_reference("fan_closed.rds"))
 })
 
 test_that("smoothing and filtering operations work", {
@@ -44,6 +44,6 @@ test_that("smoothing and filtering operations work", {
     expect_that(gaussianSmooth(data,c(1,1)), equals_reference("2d_smooth_large.rds"))
     
     kernel <- shapeKernel(c(3,3), type="diamond")
-    expect_that(meanFilter(lena,kernel), equals_reference("lena_mean_filtered.rds"))
-    expect_that(medianFilter(lena,kernel), equals_reference("lena_median_filtered.rds"))
+    expect_that(meanFilter(fan,kernel), equals_reference("fan_mean_filtered.rds"))
+    expect_that(medianFilter(fan,kernel), equals_reference("fan_median_filtered.rds"))
 })
