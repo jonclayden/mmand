1cb7d95452806646423531dd5b1003141a60dcd0
Jon Clayden <code@clayden.org>
2016-08-18 17:30:23 +0100

    Using custom iterators in Array class

--- src/Array.h
+++ src/Array.h
@@ -17,12 +17,47 @@ protected:
     std::vector<DataType> data;
     std::vector<int> dims;
     int nDims;
+    std::vector<size_t> strides;
+    
+    template <typename ValueType>
+    class IteratorType : public std::iterator<std::forward_iterator_tag, ValueType>
+    {
+    private:
+        ValueType *ptr;
+        size_t step;
+        
+    public:
+        IteratorType ()
+            : ptr(NULL), step(0) {}
+        IteratorType (ValueType *p, const size_t step = 1)
+            : ptr(p), step(step) {}
+        IteratorType (const IteratorType<ValueType> &other)
+            : ptr(other.ptr), step(other.step) {}
+        
+        IteratorType<ValueType> & operator++ () { ptr += step; return *this; }
+        IteratorType<ValueType> operator+ (ptrdiff_t n) { return IteratorType<ValueType>(ptr + n*step, step); }
+        
+        bool operator== (const IteratorType<ValueType> &other) { return (ptr==other.ptr && step==other.step); }
+        bool operator!= (const IteratorType<ValueType> &other) { return (ptr!=other.ptr || step!=other.step); }
+        
+        ValueType & operator* () { return *ptr; }
+        
+        bool valid () { return (ptr != NULL && step > 0); }
+    };
+    
+    void calculateStrides ()
+    {
+        strides = std::vector<size_t>(nDims+1);
+        strides[0] = 1;
+        for (int i=0; i<nDims; i++)
+            strides[i+1] = strides[i] * size_t(dims[i]);
+    }
 
 public:
-    typedef typename std::vector<DataType>::const_iterator const_iterator;
-    typedef typename std::vector<DataType>::iterator iterator;
-    typedef typename std::vector<DataType>::const_reference const_reference;
-    typedef typename std::vector<DataType>::reference reference;
+    typedef IteratorType<const DataType> ConstIterator;
+    typedef IteratorType<DataType> Iterator;
+    typedef const DataType & ConstReference;
+    typedef DataType & Reference;
     
     Array () { nDims = 0; }
     
@@ -30,6 +65,7 @@ public:
         : dims(dims)
     {
         nDims = dims.size();
+        calculateStrides();
         
         size_t length = 1;
         for (int i=0; i<nDims; i++)
@@ -42,6 +78,7 @@ public:
         : data(data), dims(dims)
     {
         nDims = dims.size();
+        calculateStrides();
     }
     
     size_t size () const { return data.size(); }
@@ -49,34 +86,35 @@ public:
     
     void fill (const DataType &value) { data.assign(data.size(), value); }
     
-    const_iterator begin () const { return data.begin(); }
-    iterator begin () { return data.begin(); }
-    const_iterator end () const { return data.end(); }
-    iterator end () { return data.end(); }
+    ConstIterator begin () const { return ConstIterator(&data.front()); }
+    Iterator begin () { return Iterator(&data.front()); }
+    ConstIterator end () const { return ConstIterator(&data.back() + 1); }
+    Iterator end () { return Iterator(&data.back() + 1); }
     
-    const_reference at (const size_t n) const { return data.at(n); }
-    const_reference at (const std::vector<int> &loc) const
+    ConstIterator beginLine (const std::vector<int> &begin, const int dim) const { return ConstIterator(&at(begin), strides[dim]); }
+    Iterator beginLine (const std::vector<int> &begin, const int dim) { return Iterator(&at(begin), strides[dim]); }
+    ConstIterator endLine (const std::vector<int> &begin, const int dim) const
     {
-        size_t n;
-        flattenIndex(loc, n);
-        return data.at(n);
+        const size_t offset = (dims[dim] - begin[dim]) * strides[dim];
+        return ConstIterator(&at(begin) + offset, strides[dim]);
+    }
+    Iterator endLine (const std::vector<int> &begin, const int dim)
+    {
+        const size_t offset = (dims[dim] - begin[dim]) * strides[dim];
+        return Iterator(&at(begin) + offset, strides[dim]);
     }
     
-    reference operator[] (const size_t n) { return data[n]; }
-    reference operator[] (const std::vector<int> &loc)
-    {
-        size_t n;
-        flattenIndex(loc, n);
-        return data[n];
-    }
+    ConstReference at (const size_t n) const { return data.at(n); }
+    ConstReference at (const std::vector<int> &loc) const { return data.at(flattenIndex(loc)); }
     
-    const_reference operator[] (const size_t n) const { return data[n]; }
-    const_reference operator[] (const std::vector<int> &loc) const
-    {
-        size_t n;
-        flattenIndex(loc, n);
-        return data[n];
-    }
+    Reference at (const size_t n) { return data.at(n); }
+    Reference at (const std::vector<int> &loc) { return data.at(flattenIndex(loc)); }
+    
+    ConstReference operator[] (const size_t n) const { return data[n]; }
+    ConstReference operator[] (const std::vector<int> &loc) const { return data[flattenIndex(loc)]; }
+    
+    Reference operator[] (const size_t n) { return data[n]; }
+    Reference operator[] (const std::vector<int> &loc) { return data[flattenIndex(loc)]; }
     
     const std::vector<DataType> & getData () const { return data; }
     const std::vector<int> & getDimensions () const { return dims; }
@@ -88,6 +126,13 @@ public:
     
     void flattenIndex (const std::vector<int> &loc, size_t &result) const;
     void expandIndex (const size_t &loc, std::vector<int> &result) const;
+    
+    size_t flattenIndex (const std::vector<int> &loc) const
+    {
+        size_t n;
+        flattenIndex(loc, n);
+        return n;
+    }
 };
 
 #endif
