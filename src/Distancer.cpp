984811a2d232a055287f7d89aec5fd6688134905
Jon Clayden <code@clayden.org>
2020-02-10 11:05:59 +0000

    Reworking for explicitness and to avoid edge-cases; adding more comments and the final square-root transform

--- src/Distancer.cpp
+++ src/Distancer.cpp
@@ -19,39 +19,72 @@ inline double intersectionPoint (Array<double>::Iterator &it, const int &loc, co
 
 Array<double> * Distancer::run ()
 {
-    // Transform the source array so that distances are zero within the region and infinite elsewhere
+    // Transform the source array so that distances are zero within the region
+    // and infinite elsewhere
     Array<double> *result = new Array<double>(*original);
     std::transform(original->begin(), original->end(), result->begin(), initialTransform);
     
     const std::vector<int> &dims = original->getDimensions();
     int nDims = original->getDimensionality();
     
+    // This form is separable, so we apply it in one direction at a time
     for (int i=0; i<nDims; i++)
     {
         for (size_t j=0; j<result->countLines(i); j++)
         {
-            std::vector<int> vertices(dims[i]);
-            std::vector<double> intersections(dims[i]);
+            // The vertices are the minima of a series of parabolas. The
+            // intersections are the locations where they cross
+            std::vector<int> vertices;
+            std::vector<double> intersections;
             
-            vertices[0] = 0;
-            intersections[0] = R_NegInf;
-            intersections[1] = R_PosInf;
+            // At least two parabolas are needed for a "real" intersection to
+            // occur. Parabolas k-1 and k intersect at intersections[k]. The
+            // first value here is an "off the left end" extreme
+            intersections.push_back(R_NegInf);
             
-            int k = 0;
+            // Get an iterator to access the line of data
             Array<double>::Iterator it = result->beginLine(j,i);
-            for (int l=1; l<dims[i]; l++)
-            {
-                double s = intersectionPoint(it, l, vertices[k]);
-                while (s <= intersections[k])
-                    s = intersectionPoint(it, l, vertices[--k]);
-                vertices[++k] = l;
-                intersections[k] = s;
-                intersections[k+1] = R_PosInf;
-            }
-            
-            k = 0;
             for (int l=0; l<dims[i]; l++)
             {
+                // Don't place a parabola if the transformed data is infinite
+                if (!R_FINITE(it[l]))
+                    continue;
+                
+                // If at least one other parabola has been placed, find the
+                // relevant intersection with the new parabola
+                if (!vertices.empty())
+                {
+                    // If the intersection with the most recently placed
+                    // parabola is to the "left" of its intersection with its
+                    // predecessor, the new one replaces the previous one
+                    // (and so on, back through the chain)
+                    double s = intersectionPoint(it, l, vertices.back());
+                    while (s <= intersections.back())
+                    {
+                        vertices.pop_back();
+                        intersections.pop_back();
+                        s = intersectionPoint(it, l, vertices.back());
+                    }
+                    intersections.push_back(s);
+                }
+                
+                // Place the new parabola, centred at l
+                vertices.push_back(l);
+            }
+            
+            // Add an "off the right end" extreme value for use below
+            intersections.push_back(R_PosInf);
+            
+            // If no parabolas have been placed, there's nothing else to do
+            if (vertices.empty())
+                continue;
+            
+            // Step back over the data, replacing each element with the value
+            // of the lowest parabola at that location
+            for (int k=0, l=0; l<dims[i]; l++)
+            {
+                // The relevant parabola is the last one whose intersection
+                // point we haven't yet passed
                 double q = static_cast<double>(l);
                 while (intersections[k+1] < q)
                     k++;
@@ -61,5 +94,8 @@ Array<double> * Distancer::run ()
         }
     }
     
+    // Take the square-root of each value to get Euclidean distance. The
+    // function pointer cast is needed to resolve the overload on sqrt()
+    std::transform(result->begin(), result->end(), result->begin(), (double(*)(double)) ::sqrt);
     return result;
 }
