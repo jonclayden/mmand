8d2aa8910e40ba505d7a8ff57ebcbafe5476376f
Jon Clayden <code@clayden.org>
2020-02-27 11:16:52 +0000

    Running display() test from the temporary directory to avoid creating files in the package library

--- inst/tinytest/test-35-display.R
+++ inst/tinytest/test-35-display.R
@@ -1,6 +1,9 @@
 B <- loder::readPng(system.file("images", "B.png", package="mmand"))
 
+# We need to avoid creating plot files within installed package directories
+setwd(tempdir())
 expect_null(display(B))
+
 output <- capture.output(sketch(B))
 expect_true(any(grepl("@@@@@", output, fixed=TRUE)))
 expect_false(all(grepl("@@@@@", output, fixed=TRUE)))
